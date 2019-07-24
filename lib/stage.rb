4f799e87a34edc359e40db75972be1188bb63308
Jess Doss and Tae Lee <tae@>
2019-07-24 13:25:23 -0700

    This is basic of stage class

---
+++ lib/stage.rb
@@ -0,0 +1,32 @@
+require('pry')
+
+class Stage
+  attr_reader :id
+  attr_accessor :name, :artist
+  @@statges = {}
+  @@total_rows = 0
+
+  def initialize(name, artist, id)
+    @name = name
+    @artist = artist
+    @id = id || @@total_rows += 1
+  end
+
+  def self.all
+    @@stages.values()
+  end
+
+  def create
+    @@stages[self.id] = Stage.new(self.name, self.artist, self.id)
+    Stage.sort
+  end
+
+  def delete
+    @@stages.delete(self.id)
+  end
+
+  def update(name, artist)
+    self.name = name
+    self.artist = artist
+    @@stages[self.id] = Stage.new(self.name, self.artist, self.id)
+  end
